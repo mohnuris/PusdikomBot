a6ae36b349c5fefe4e20914eb6bd38805a509e9e
nuris.ft.unuja@gmail.com <nuris.ft.unuja@gmail.com>
2020-12-22 23:52:11 +0700

    capek

--- public/index.js
+++ public/index.js
@@ -42,6 +42,7 @@ bot.hears('babi', (ctx) => ctx.reply("Perkataan Anda tidak pantas"));
 bot.hears('apa kabar', (ctx) => ctx.reply("saya baik, kamu..?"));
 bot.hears('baik', (ctx) => ctx.reply("alhamdulilah, semooga harimu menyenangkan"));
 bot.hears('tidak baik', (ctx) => ctx.reply("kenapa?"));
+bot.hears('capek', (ctx) => ctx.reply("mungkin kamu lelah"));
 bot.hears('iyakah', (ctx) => ctx.reply("beneran saya baik, kamu bagaimana..?"));
 bot.hears('saya baik', (ctx) => ctx.reply("alhamdulilah ☺️"));
 bot.hears('😘', (ctx) => ctx.reply("🥰"));
